7fd5aed882f44e502f7c47729fe78a5644d09fe6
jackydhc <dhc52342@163.com>
2019-07-16 10:37:36 +0800

    更新渐变

--- app/src/main/java/com/ian/chartdemo/MainActivity.java
+++ app/src/main/java/com/ian/chartdemo/MainActivity.java
@@ -2,6 +2,8 @@ package com.ian.chartdemo;
 
 import android.graphics.Color;
 import android.graphics.DashPathEffect;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,16 +12,22 @@ import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IFillFormatter;
+import com.github.mikephil.charting.interfaces.dataprovider.LineDataProvider;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.utils.Utils;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private LineChart chartview;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        LineChart chartview = findViewById(R.id.chatview);
+        chartview = findViewById(R.id.chatview);
 
         setupChart(chartview,getData(36,100),Color.parseColor("#000088"));
 
@@ -83,7 +91,7 @@ public class MainActivity extends AppCompatActivity {
         LineDataSet set1 = new LineDataSet(values, "DataSet 1");
         // set1.setFillAlpha(110);
         set1.setFillColor(Color.RED);
-
+        set1.enableDashedLine(10f, 5f, 0f);
         set1.setLineWidth(1.75f);
         set1.setCircleRadius(5f);
         set1.setCircleHoleRadius(2.5f);
@@ -96,6 +104,20 @@ public class MainActivity extends AppCompatActivity {
         set1.setFormLineDashEffect(new DashPathEffect(new float[]{10f, 5f}, 0f));
         set1.setFormSize(15.f);
         set1.setDrawFilled(true);
+        set1.setFillFormatter(new IFillFormatter() {
+            @Override
+            public float getFillLinePosition(ILineDataSet dataSet, LineDataProvider dataProvider) {
+                return chartview.getAxisLeft().getAxisMinimum();
+            }
+        });
+        if (Utils.getSDKInt() >= 18) {
+            // drawables only supported on api level 18 and above
+            Drawable drawable = ContextCompat.getDrawable(this, R.drawable.fade_red);
+            set1.setFillDrawable(drawable);
+        } else {
+            set1.setFillColor(Color.BLACK);
+        }
+
         // create a data object with the data sets
         return new LineData(set1);
     }
